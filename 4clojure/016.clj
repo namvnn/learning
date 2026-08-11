40cad0a01780a9ea9976e1dc9eeeba38d107c947
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-08-12 00:21:41 +1000

    4clojure: 7-19

---
+++ 4clojure/016.clj
@@ -0,0 +1,5 @@
+(= ((fn [name] (str "Hello, " name "!")) "Dave") "Hello, Dave!")
+
+(= ((fn [name] (str "Hello, " name "!")) "Jenn") "Hello, Jenn!")
+
+(= ((fn [name] (str "Hello, " name "!")) "Rhea") "Hello, Rhea!")
