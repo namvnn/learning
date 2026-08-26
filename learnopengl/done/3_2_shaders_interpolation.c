cc589c187ab91c4b210fad0370606c6d47365d3b
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-08-26 22:48:41 +1000

    learnopnegl: create shader functions

--- learnopengl/done/3_2_shaders_interpolation.c
+++ learnopengl/done/3_2_shaders_interpolation.c
@@ -1,6 +1,5 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <math.h>
 #include <stdio.h>
 
 const unsigned int SCR_WIDTH = 800;
--- learnopengl/main.c
+++ learnopengl/main.c
@@ -1,8 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -27,17 +28,18 @@ const char *fragment_shader_source =
 
 // glfw: whenever the window size changed (by OS or user resize) this callback
 // function executes
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 // glfw: process all input: query GLFW whether relevant keys are
 // pressed/released this frame and react accordingly
-void process_input(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-}
+void process_input(GLFWwindow *window);
+
+unsigned int shader_create(const char *vertex_path, const char *fragment_path);
+void shader_use(unsigned int program);
+void shader_set_bool(unsigned int program, const char *name, bool value);
+void shader_set_int(unsigned int program, const char *name, int value);
+void shader_set_float(unsigned int program, const char *name, float value);
+void shader_check_compile_errors(unsigned int shader, const char *type);
 
 int main(void) {
     // glfw: initialize and configure
@@ -122,12 +124,20 @@ int main(void) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // set position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+    glVertexAttribPointer(0,
+                          3,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          6 * sizeof(float),
                           (void *)0);
     glEnableVertexAttribArray(0);
 
     // set color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+    glVertexAttribPointer(1,
+                          3,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          6 * sizeof(float),
                           (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
@@ -162,4 +172,111 @@ int main(void) {
     glfwTerminate();
 
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+unsigned int shader_create(const char *vertex_path, const char *fragment_path) {
+    FILE *vertex_fp = fopen(vertex_path, "r");
+    fseek(vertex_fp, 0, SEEK_END);
+    long vertex_bufsize = ftell(vertex_fp);
+    char *vertex_shader_source = malloc(sizeof(char) * (vertex_bufsize + 1));
+    fseek(vertex_fp, 0, SEEK_SET);
+    size_t vertex_fread_len =
+        fread(vertex_shader_source, sizeof(char), vertex_bufsize, vertex_fp);
+    vertex_shader_source[vertex_fread_len++] = '\0';
+    fclose(vertex_fp);
+
+    FILE *fragment_fp = fopen(fragment_path, "r");
+    fseek(fragment_fp, 0, SEEK_END);
+    long fragment_bufsize = ftell(fragment_fp);
+    char *fragment_shader_source =
+        malloc(sizeof(char) * (fragment_bufsize + 1));
+    fseek(fragment_fp, 0, SEEK_SET);
+    size_t fragment_fread_len = fread(fragment_shader_source,
+                                      sizeof(char),
+                                      fragment_bufsize,
+                                      fragment_fp);
+    fragment_shader_source[fragment_fread_len++] = '\0';
+    fclose(fragment_fp);
+
+    unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex,
+                   1,
+                   (const char *const *)(&vertex_shader_source),
+                   NULL);
+    glCompileShader(vertex);
+    shader_check_compile_errors(vertex, "VERTEX");
+
+    unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment,
+                   1,
+                   (const char *const *)(&fragment_shader_source),
+                   NULL);
+    glCompileShader(fragment);
+    shader_check_compile_errors(fragment, "FRAGMENT");
+
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    shader_check_compile_errors(program, "PROGRAM");
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    free(vertex_shader_source);
+    free(fragment_shader_source);
+
+    return program;
+}
+
+void shader_use(unsigned int program) {
+    glUseProgram(program);
+}
+
+void shader_set_bool(unsigned int program, const char *name, bool value) {
+    glUniform1i(glGetUniformLocation(program, name), value);
+}
+
+void shader_set_int(unsigned int program, const char *name, int value) {
+    glUniform1i(glGetUniformLocation(program, name), value);
+}
+
+void shader_set_float(unsigned int program, const char *name, float value) {
+    glUniform1f(glGetUniformLocation(program, name), value);
+}
+
+void shader_check_compile_errors(unsigned int id, const char *type) {
+    int success;
+    char info_log[1024];
+
+    if (strcmp(type, "PROGRAM") == 0) {
+        glGetProgramiv(id, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(id, 1024, NULL, info_log);
+            printf(
+                "ERROR::PROGRAM_LINKING_ERROR of type: %s\n%s\n "
+                "--------------------------------------------------- -- \n",
+                type,
+                info_log);
+        }
+    } else {
+        glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(id, 1024, NULL, info_log);
+            printf(
+                "ERROR::SHADER_COMPILATION_ERROR of type: %s\n%s\n "
+                "--------------------------------------------------- -- \n",
+                type,
+                info_log);
+        }
+    }
 }
